9afabcc6766537794dade48afcd896713d2383be
Breyten Ernsting <bje@dds.nl>
2014-10-20 16:01:37 +0200

    Test the source json to see if we can load all modules. Fixes #61

--- tests/test_ocd_backend_utils_misc.py
+++ tests/test_ocd_backend_utils_misc.py
@@ -9,7 +9,7 @@ from mock import MagicMock
 from nose.tools import raises
 
 import ocd_backend
-from ocd_backend.utils.misc import load_sources_config
+from ocd_backend.utils.misc import load_sources_config, load_object
 
 
 def test_load_sources_config_filename():
@@ -21,3 +21,13 @@ def test_load_sources_config_open_file():
     with open('ocd_backend/sources.json') as json_file:
         sources = load_sources_config(json_file)
         assert isinstance(sources, list)
+
+def test_load_all_sources():
+    sources = load_sources_config('ocd_backend/sources.json')
+    for source in sources:
+        yield _load_source, source
+
+def _load_source(source):
+    for phase in ['extractor', 'transformer', 'item', 'loader']:
+        module = load_object(source[phase])
+    
